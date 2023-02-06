7a2746d0e3d374342bcdba5b4c019e1b0a06f0a4
iliassanati <iliassanati@gmail.com>
2023-02-06 16:40:37 +0100

    first cell

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,3 +1,4 @@
+import './preview.css';
 import { useEffect, useRef } from 'react';
 
 interface PreviewProps {
@@ -10,13 +11,16 @@ const html = `
 <body>
 <div id="root"></div>
 <script>
+const handleError = error => {
+  const root = document.querySelector('#root');
+  root.innerHTML = '<div style="color:red"><h4>Runtime Error</h4>' + error + '</div>';
+console.error(error);
+}
 window.addEventListener('message', (event)=>{
   try {
     eval(event.data);
   } catch (error) {
-    const root = document.querySelector('#root');
-    root.innerHTML = '<div style="color:red"><h4>Runtime Error</h4>' + error + '</div>';
-  console.error(error);
+handleError(error)
   }
 },false)
 </script>
@@ -29,17 +33,20 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-
-    iframe.current.contentWindow.postMessage(code, '*');
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*');
+    });
   }, [code]);
 
   return (
-    <iframe
-      title='preview'
-      ref={iframe}
-      sandbox='allow-scripts'
-      srcDoc={html}
-    />
+    <div className='preview-wrapper'>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,5 +1,6 @@
-import React, { PropsWithChildren } from 'react';
-import { ResizableBox } from 'react-resizable';
+import React, { PropsWithChildren, useEffect, useState } from 'react';
+import './resizable.css';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 
 interface ResizableProps {
   direction: 'horizontal' | 'vertical';
@@ -9,11 +10,55 @@ const Resizable: React.FC<PropsWithChildren<ResizableProps>> = ({
   direction,
   children,
 }) => {
-  return (
-    <ResizableBox height={300} width={300}>
-      {children}
-    </ResizableBox>
-  );
+  let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth * 0.75);
+
+  useEffect(() => {
+    let timer: any;
+    const listener = () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      timer = setTimeout(() => {
+        setInnerHeight(window.innerHeight);
+        setInnerWidth(window.innerWidth);
+        if (window.innerWidth * 0.75 < width) {
+          setWidth(window.innerWidth * 0.75);
+        }
+      }, 100);
+    };
+    window.addEventListener('resize', listener);
+
+    return () => {
+      window.removeEventListener('resize', listener);
+    };
+  }, []);
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      minConstraints: [innerWidth * 0.2, Infinity],
+      maxConstraints: [innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width,
+      resizeHandles: ['e'],
+      onResizeStop(e, data) {
+        setWidth(data.size.width);
+      },
+    };
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ['s'],
+    };
+  }
+
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
